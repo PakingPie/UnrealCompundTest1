93f15f425cfe9dc3ef2cf8cc9eaf459cef27a285
PakingPie <john.l.p@live.com>
2026-03-06 10:44:20 +0800

    Minor Updates

--- Content/Materials/Temp.hlsl
+++ Content/Materials/Temp.hlsl
@@ -25,13 +25,107 @@ for(int i = 0; i < MaxSteps; i++)
             
             shadowDist += lightSample;
         }
+
+        float3 distFieldPos = 2 * (CurrPos - 0.5) * GetPrimitiveData(Parameters).LocalObjectBoundsMax;
+        distFieldPos = LWCToFloat(TransformLocalPositionToWorld(Parameters, distFieldPos)) - CameraPosWS;
+        float distFieldTraceDist = 1;
+        float distFieldShadow = 1;
+        float currDist = 0;
+        float distAlongTrace = 0;
+
+        for(int d = 0; d < DistanceFieldShadowSteps; d++)
+        {
+            distAlongTrace += currDist;
+            currDist = GetDistanceToNearestSurfaceGlobal(distFieldPos);
+            
+            float sphereSize = distAlongTrace * LightTangent;
+
+            distFieldShadow = min(saturate(currDist / sphereSize), distFieldShadow);
+
+            distFieldPos += LightVectorWS * distFieldTraceDist * currDist;
+            distFieldTraceDist *= 1.0001;
+        }
+
     
         currSample = 1 - exp(-currSample * Density);
-        lightEnergy += exp(-shadowDist * ShadowDensity) * currSample * transmittance;
+        lightEnergy += exp(-shadowDist * ShadowDensity) * currSample * transmittance * LightColor * distFieldShadow;
         transmittance *= 1.0 - currSample;
+
+        shadowDist = 0;
+		
+        lightPos = CurrPos + float3(0,0,0.025);
+        float lightSample = PseudoVolumeTexture(Tex, TexSampler, saturate(lightPos), XYFrames, NumFrames).r;
+        shadowDist += lightSample;
+        lightPos = CurrPos + float3(0,0,0.05);
+        lightSample = PseudoVolumeTexture(Tex, TexSampler, saturate(lightPos), XYFrames, NumFrames).r;
+        shadowDist += lightSample;
+        lightPos = CurrPos + float3(0,0,0.15);
+        lightSample = PseudoVolumeTexture(Tex, TexSampler, saturate(lightPos), XYFrames, NumFrames).r;
+        shadowDist += lightSample;
+
+        lightEnergy    += exp(-shadowDist  * AmbientDensity) * currSample * SkyColor * transmittance;
     }
 
     CurrPos += -LocalCamVec;
+}
+
+CurrPos += LocalCamVec * (1 - FinalStepSize);
+float currSample = PseudoVolumeTexture(Tex, TexSampler, saturate(CurrPos), XYFrames, NumFrames).r;
+
+if(currSample > 0.001)
+{
+    float3 lightPos = CurrPos;
+    float shadowDist = 0;
+    for(int s = 0; s < ShadowSteps; s++)
+    {
+            lightPos += LightVector;
+            float lightSample = PseudoVolumeTexture(Tex, TexSampler, saturate(lightPos), XYFrames, NumFrames).r;
+
+            float3 shadowBoxTest = floor(0.5 + abs(0.5 - lightPos));
+            float exitShadowBox = shadowBoxTest.x + shadowBoxTest.y + shadowBoxTest.z;
+            if(shadowDist > shadowThreshold || exitShadowBox >= 1) break;
+            
+            shadowDist += lightSample;
+    }
+
+    float3 distFieldPos = 2 * (CurrPos - 0.5) * GetPrimitiveData(Parameters).LocalObjectBoundsMax;
+    distFieldPos = LWCToFloat(TransformLocalPositionToWorld(Parameters, distFieldPos)) - CameraPosWS;
+    float distFieldTraceDist = 1;
+    float distFieldShadow = 1;
+    float currDist = 0;
+    float distAlongTrace = 0;
+
+    for(int d = 0; d < DistanceFieldShadowSteps; d++)
+    {
+        distAlongTrace += currDist;
+        currDist = GetDistanceToNearestSurfaceGlobal(distFieldPos);
+        
+        float sphereSize = distAlongTrace * LightTangent;
+
+        distFieldShadow = min(saturate(currDist / sphereSize), distFieldShadow);
+
+        distFieldPos += LightVectorWS * distFieldTraceDist * currDist;
+        distFieldTraceDist *= 1.0001;
+    }
+
+
+    currSample = 1 - exp(-currSample * Density);
+    lightEnergy += exp(-shadowDist * ShadowDensity) * currSample * transmittance * LightColor * distFieldShadow;
+    transmittance *= 1.0 - currSample;
+
+    shadowDist = 0;
+    
+    lightPos = CurrPos + float3(0,0,0.025);
+    float lightSample = PseudoVolumeTexture(Tex, TexSampler, saturate(lightPos), XYFrames, NumFrames).r;
+    shadowDist += lightSample;
+    lightPos = CurrPos + float3(0,0,0.05);
+    lightSample = PseudoVolumeTexture(Tex, TexSampler, saturate(lightPos), XYFrames, NumFrames).r;
+    shadowDist += lightSample;
+    lightPos = CurrPos + float3(0,0,0.15);
+    lightSample = PseudoVolumeTexture(Tex, TexSampler, saturate(lightPos), XYFrames, NumFrames).r;
+    shadowDist += lightSample;
+
+    lightEnergy    += exp(-shadowDist  * AmbientDensity) * currSample * SkyColor * transmittance;
 }
 
 // CurrPos -= LocalCamVec * StepSize;
